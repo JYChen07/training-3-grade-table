a934e27cf41d42a2e6c22f18214e894fa81bb50e
JYChen07 <jamesyufanchen@gmail.com>
2021-11-28 11:12:30 +0100

    added a working add function with no checks on varaibles

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function helloworld(){
-  console.log("Hello world");
-};
+function addStudent() {
+    const student = document.getElementById("student").value;
+    const grade = document.getElementById("grade").value;
+    const table = document.getElementById("gradeTable");
+    const row_length = document.getElementById("gradeTable").rows.length;
+    const row = table.insertRow(row_length-1);
+    const cell_0 = row.insertCell(0);
+    const cell_1 = row.insertCell(1);
+    const cell_2 = row.insertCell(2);
+    cell_0.innerHTML = student;
+    cell_1.innerHTML = grade;
+    cell_2.innerHTML = "*";
+
+}
