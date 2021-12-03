9998b4bd5f5a1faca7bcf9ca0b2fe41adc1ad075
JYChen07 <jamesyufanchen@gmail.com>
2021-12-03 11:48:06 +0100

    updated verification functions

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
+
+function verifyName(name) { //verifies if there is a Student Name
+    if (typeof name === 'string' && name != "") {
+        return true;
+    }
+    return false;
+}
+
+function verifyGrade(grade) { //verifies if there is a valid grade
+    if (typeof grade === 'number' && grade >= 0 && grade <= 100) {
+        return true;
+    }
+    return false;
+}
+
 /*
  * addStudent adds a new row with the Student Name and Grade to the table.
  * 
  */
 function addStudent() {
     const student = document.getElementById("student").value;
-    const grade = document.getElementById("grade").value;
+    const grade = parseInt(document.getElementById("grade").value, 10);
     const table = document.getElementById("gradeTable");
     const row_length = document.getElementById("gradeTable").rows.length;
 
-    if (student!= "" && grade >= 0 && grade <= 100) {
+    let verify_student = verifyName(student);
+    let verify_grade = verifyGrade(grade);
+    //console.log(verify_student + " - " + verify_grade);
+    console.log(grade);
+    if (verify_student && verify_grade) {
         //checks if there is something inputted in student text box 
         //as well as a value of 0 to 100 for grade score
         const row = table.insertRow(row_length - 1);
@@ -20,15 +39,15 @@ function addStudent() {
         cell_2.innerHTML = "";
         document.getElementById("check").innerHTML = "";
     }
-    else if (student == "" && (grade < 0 || grade > 100 || grade == "")) {
+    else if (!verify_student && !verify_grade) {
         //if there is no valid student and grade print out an error 
         document.getElementById("check").innerHTML = "No student and proper grade inputs.";
     }
-    else if (student == "") {
+    else if (!verify_student) {
         //only invalid student string
         document.getElementById("check").innerHTML = "No student name has been inputted.";
     }
-    else {
+    else if (!verify_grade) {
         //only invalid grade score
         document.getElementById("check").innerHTML = "Out of bounds grade input.";
     }
